29323d9b1edbbc48641dfaeef1689a18e66a6a5c
Nekrasov Ilya <nekrasov.ilya90@gmail.com>
2021-07-21 23:34:21 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ return [
     'connection' => [
 
         /*
-         * Определяет хосты для подключения в формате http[s]://[user][:pass]@hostname[:9200]
+         * Elasticsearch hosts in format http[s]://[user][:pass]@hostname[:9200]
          */
         'hosts' => explode(',', env('ELASTICSEARCH_HOSTS')),
     ]
