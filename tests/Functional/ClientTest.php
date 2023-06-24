2a0d420e3133348ba533c02c8c64204ba1fc14a7
Alex Krol <crol@bk.ru>
2023-06-24 14:49:23 +0300

    #105514 Метод получения информации по индексам

--- tests/Functional/ClientTest.php
+++ tests/Functional/ClientTest.php
@@ -3,11 +3,14 @@
 namespace Ensi\LaravelElasticQuery\Tests\Functional;
 
 use Ensi\LaravelElasticQuery\ElasticClient;
+use Ensi\LaravelElasticQuery\Tests\AssertsArray;
 use Ensi\LaravelElasticQuery\Tests\Models\ProductsIndex;
 use Ensi\LaravelElasticQuery\Tests\Seeds\ProductIndexSeeder;
 
 class ClientTest extends ElasticTestCase
 {
+    use AssertsArray;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -17,8 +20,29 @@ class ClientTest extends ElasticTestCase
 
     public function testCatIndices(): void
     {
-        $results = $this->newClient()->catIndices(ProductsIndex::fullName());
+        $response = $this->newClient()->catIndices(ProductsIndex::fullName());
 
+        $this->assertGreaterThanOrEqual(1, count($response));
+        $this->assertArrayStructure([[
+            'index',
+            'status',
+            'health',
+            'uuid',
+            'pri',
+            'rep',
+            'docs.count',
+            'docs.deleted',
+            'store.size',
+            'pri.store.size',
+        ]], $response);
+    }
+
+    public function testCatIndicesOnlySpecifiedFields(): void
+    {
+        $response = $this->newClient()->catIndices(ProductsIndex::fullName(), ['index', 'status']);
+
+        $this->assertArrayStructure([['index', 'status']], $response);
+        $this->assertArrayNotHasKey('health', $response[0]);
     }
 
     private function newClient(): ElasticClient
