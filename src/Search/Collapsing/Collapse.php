08f2957f6878b9838a1628d2508a8cf5e4744d5c
Kapitanov Andrey <kapitanov@greensight.ru>
2024-02-09 13:59:25 +0300

    Collaps Inner Hits and Search Aggregation

--- src/Search/Collapsing/Collapse.php
+++ src/Search/Collapsing/Collapse.php
@@ -5,10 +5,16 @@ namespace Ensi\LaravelElasticQuery\Search\Collapsing;
 use Ensi\LaravelElasticQuery\Contracts\DSLAware;
 use Webmozart\Assert\Assert;
 
+/**
+ * @property string $field
+ * @property InnerHits[] $innerHits
+ */
 class Collapse implements DSLAware
 {
-    public function __construct(private string $field)
-    {
+    public function __construct(
+            private string $field,
+            private array $innerHits = [],
+    ) {
         Assert::stringNotEmpty(trim($field));
     }
 
@@ -19,6 +25,13 @@ class Collapse implements DSLAware
 
     public function toDSL(): array
     {
-        return ['field' => $this->field];
+        $dsl = ['field' => $this->field];
+
+        /** @var InnerHits $innerHit */
+        foreach ($this->innerHits as $innerHit) {
+            $dsl['inner_hits'][] = $innerHit->toDSL();
+        }
+
+        return $dsl;
     }
 }
--- src/Search/Page.php
+++ src/Search/Page.php
@@ -9,6 +9,7 @@ class Page
     public function __construct(
         public int $size,
         public Collection $hits,
+        public Collection|null $aggs,
         public int $offset = 0,
         public int $total = 0
     ) {
